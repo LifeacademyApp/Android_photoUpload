a2ff5a963b46b555712395fc31d4a086511e6bba
Alger <gino63o7@gmail.com>
2018-03-22 16:38:19 +0800

    Implement taking photo, storing and main view.

--- AndroidPractice/app/src/main/java/com/example/yolo/androidpractice/MainActivity.java
+++ AndroidPractice/app/src/main/java/com/example/yolo/androidpractice/MainActivity.java
@@ -1,55 +1,147 @@
 package com.example.yolo.androidpractice;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Environment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.provider.MediaStore;
 import android.content.Intent;
 import android.util.Log;
-import 	android.view.View;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 import android.net.Uri;
-import 	java.io.File;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import android.support.v4.content.FileProvider;
 
 public class MainActivity extends AppCompatActivity {
-
+    static final int REQUEST_TAKE_PHOTO = 1;
+    String mCurrentPhotoPath;
     Uri imgUri;
     ImageView imv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        imv = (ImageView) findViewById(R.id.imageView);
+        //imv = (ImageView) findViewById(R.id.imageView);
     }
+
     public void onGet(View v){
-        //? String dir = Environment.getExternalStorageDirectory().getAbsolutePath();
-        //Log.i("9999999", "yoooo");
-        String dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString();
-        String fname = "p" + System.currentTimeMillis() + ".jpg";
-        //imgUri = Uri.parse("file:///SUGAR Y12/DCIM/Camera/" + fname);
-        imgUri = Uri.parse("file://" + dir + "/" + fname);
+        dispatchTakePictureIntent();
 
 
-        Intent it = new Intent("android.media.action.IMAGE_CAPTURE");
-        it.putExtra(MediaStore.EXTRA_OUTPUT, imgUri);
-        //startActivity(it);
-        startActivityForResult(it, 100);
+      //  Log.v("yooooooo","tesettest");
+//        String dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString();
+//        String fname = "p" + System.currentTimeMillis() + ".jpg";
+//        imgUri = Uri.parse("file://" + dir + "/" + fname);
+//
+//
+//        Intent it = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        it.putExtra(MediaStore.EXTRA_OUTPUT, imgUri);
+//        startActivityForResult(it, REQUEST_TAKE_PHOTO);
     }
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+    //? 當一個 activity return 後，便會來到此
+    //? 相機程式大概也是個 activity
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(resultCode == Activity.RESULT_OK && requestCode == 100){
-            Bitmap bmp = BitmapFactory.decodeFile(imgUri.getPath());
-            imv.setImageBitmap(bmp);
+        if(resultCode == Activity.RESULT_OK && requestCode == REQUEST_TAKE_PHOTO){
+           // showImg();
+            galleryAddPic();                // 通知系統有新的照片
+            dispatchTakePictureIntent();
         }
         else{
-            Toast.makeText(this, "沒有拍到照片", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "取消拍照", Toast.LENGTH_LONG).show();
         }
     }
+    void showImg(){
+        int iw, ih, vw, vh;
+
+        BitmapFactory.Options option = new BitmapFactory.Options();
+        option.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, option);
+        iw = option.outWidth;
+        ih = option.outHeight;
+
+        vw = imv.getWidth();
+        vh = imv.getHeight();
+
+        int scaleFactor = Math.min(iw/vw, ih/vh);
+
+        option.inJustDecodeBounds = false;
+        option.inSampleSize = scaleFactor;
+
+        Bitmap bmp = BitmapFactory.decodeFile(mCurrentPhotoPath, option);
+        imv.setImageBitmap(bmp);
+    }
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        //File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) ;
+        // Log.d("MainActivity", "Environment.DIRECTORY_PICTURES");
+        // 建出暫存檔
+        //File.
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            //File photoFile = null;
+            String dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString();
+            String fname = "p" + System.currentTimeMillis() + ".jpg";
+            mCurrentPhotoPath = dir + "/" + fname;
+            File photoFile = new File(mCurrentPhotoPath);
+
+//            try {
+//                photoFile = createImageFile();
+//            } catch (IOException ex) {
+//                // Error occurred while creating the File
+//            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                // 確認一下 在哪行程式碼創建了檔案的，並試試看原本非 provider 的
+                // 方法是否也會創建空的檔案
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
 }
